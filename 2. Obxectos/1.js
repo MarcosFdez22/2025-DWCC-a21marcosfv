a1d686125df7b5914e315ea2b6ff456de8ff9fd8
MarcosFdez22 <a21marcosfv@iessanclemente.net>
2025-09-29 17:33:01 +0200

    2.Obxectos

---
+++ 2. Obxectos/1.js
@@ -0,0 +1,11 @@
+"use strict";
+//////////////////////////////////////
+console.log("Exercicio 1");
+const cadea = "desenvolvemento web";
+
+let novaCadea = cadea.slice(0, 1).toUpperCase() + cadea.slice(1, 19);
+
+console.log(novaCadea); // 'Desenvolvemento web'
+console.log("\n");
+//////////////////////////////////////
+console.log("Exercicio 2");
